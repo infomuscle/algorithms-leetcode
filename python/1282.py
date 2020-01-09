d2f9d54afbe97c58cba6d479fb6d7cfa63c0654c
infomuscle <infomuscle10@gmail.com>
2020-01-09 18:31:44 +0900

    Group the People Given the Group Size They Belong To(Solving)

---
+++ python/1282.py
@@ -0,0 +1,31 @@
+# class Solution:
+#     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+#         return
+
+
+groupSizes1 = [3,3,3,3,3,1,3]
+groupSizes2 = [2,1,3,3,3,2]
+
+
+answers = []
+dict = {}
+
+for i in range(len(groupSizes1)):
+    isKey = True
+
+    # dict의 키 여부 체크
+    # if (groupSizes1[i] in dict):
+
+
+    # 없으면 키 : 리스트 생성
+
+    # 키 있고 자리 없으면 answers에 append
+    # answers.append()후 키 리스트 초기화
+
+
+    # 리스트에 ID append
+    dict[i] = groupSizes1[i]
+
+
+
+# 마지막에 꽉 찬 리스트 체크
