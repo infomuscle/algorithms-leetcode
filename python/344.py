f4a160c7713a14127f5ffcb82e13ddf5cdfd736d
infomuscle <infomuscle10@gmail.com>
2020-01-10 14:58:52 +0900

    Reverse String

--- python/344.py
+++ python/344.py
@@ -1,13 +1,25 @@
-# 제출 코드 - Runtime 93.53 Memory 100
-
+# 제출 코드 - Runtime 99.32 Memory 100
 class Solution:
     def reverseString(self, s):
         s.reverse()
+        print(s)
+        # s.reverse()
+
+
 
 sample1 = ["h","e","l","l","o"]
 sample2 = ["H","a","n","n","a","h"]
 
 sol = Solution()
+sol.reverseString(sample1)
+sol.reverseString(sample2)
 
-print(sol.reverseString(sample1))
-print(sol.reverseString(sample2))
+# reverser(sample1)
+# reverser(sample2)
+
+
+### Code 1 - 성
+# def reverser(l):
+#     for i in range(len(l)-1):
+#         l.insert(i, l.pop())
+#     print(l)
