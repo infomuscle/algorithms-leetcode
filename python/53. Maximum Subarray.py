9373c4ebd0ced6de8a8ccceeb8257805cf4568a9
infomuscle <infomuscle10@gmail.com>
2020-01-23 18:24:29 +0900

    Solving

--- python/53. Maximum Subarray.py
+++ python/53. Maximum Subarray.py
@@ -2,49 +2,30 @@ class Solution:
     def tmaxSubArray(self, nums):
 
         i = 0
-        j = 1
+        j = 0
 
         max = nums[i]
-        
         tmax = nums[i]
-        idx = []
-
-
-        if len(nums) == 1:
-            return nums[0]
 
         while True:
-            # print("i : %d, j : %d" % (i,j))
-            # print(tmax)
             if i == len(nums) or j == len(nums):
                 break
 
+            if tmax + nums[j+1] > tmax:
+                tmax += nums[j+1]
+                j += 1
+            elif tmax - nums[i] > tmax:
+                tmax -= nums[i]
+                i += 1
+            elif tmax + nums[j+1] > tmax - nums[i]:
+                tmax += nums[j+1]
+                tmax -= nums[i]
+                i += 1
+                j += 1
 
-            if j == 1:
-                if tmax + nums[j] > tmax:
-                    tmax += nums[j]
-                    j += 1
-                else:
-                    tmax -= nums[i]
-                    i += 1
-            elif j == len(nums) -1:
-                if tmax -nums[i] > tmax:
-                    tmax -= nums[i]
-                    i += 1
-                else:
-                    j += 1
-            else:
-                if tmax + nums[j] > tmax - nums[i]:
-                    tmax += nums[j]
-                    j += 1
-                else:
-                    tmax -= nums[i]
-                    i += 1
             if tmax > max:
                 max = tmax
-                idx = [i,j]
 
-        # print(idx)
         return max
 
 tc1 = [-2,1,-3,4,-1,2,1,-5,4]
