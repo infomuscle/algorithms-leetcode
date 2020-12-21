0afb2c958a14c9f4edcd583abb6bb21679504c8a
infomuscle <infomuscle10@gmail.com>
2020-12-21 22:21:00 +0900

    Runtime 52.36 Memory 65.41

--- leetcode-python/3. Longest Substring Without Repeating Characters.py
+++ leetcode-python/3. Longest Substring Without Repeating Characters.py
@@ -1,54 +1,40 @@
+# 제출 코드 - Runtime 52.36 Memory 65.41
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        left = 0
-        right = 1
-        longest = ""
 
-        if s == "":
-            return len(s)
+        answer = 0
 
-        temp = ""
-        while True:
-            if len(temp) == 0:
-                temp += s[left]
+        chars = []
+
+        subs = ""
+        for i, c in enumerate(s):
+            if c not in chars:
+                chars.append(c)
+                subs += c
             else:
-                if s[right] not in temp:
-                    temp += s[right]
-                    right += 1
-                else:
-                    while s[left] != s[right]:
-                        left += 1
-                    left += 1
-                    temp = ""
+                subs = subs[subs.find(c) + 1:] + c
+                chars = chars[chars.index(c) + 1:]
+                chars.append(c)
 
+            if len(subs) > answer:
+                answer = len(subs)
 
-            if len(temp) > len(longest):
-                longest = temp
-            print("temp: " + temp + "   longest: " +longest + "   left: " + str(left) + "   right: " + str(right))
-            if len(s) == right:
-                break
-        return len(longest)
+        return answer
 
 
 tc1 = "abcabcbb"
 tc2 = "bbbbb"
 tc3 = "pwwkew"
 tc4 = ""
-tc5 = " "
-tc6 = "au"
-tc7 = "dvdf"
+tc5 = "au"
+tc6 = "dvdf"
+tc7 = "  "
 
 sol = Solution()
-# print(sol.lengthOfLongestSubstring(tc1))
-# print(sol.lengthOfLongestSubstring(tc2))
-# print(sol.lengthOfLongestSubstring(tc3))
-# print(sol.lengthOfLongestSubstring(tc4))
-# print(sol.lengthOfLongestSubstring(tc5))
-# print(sol.lengthOfLongestSubstring(tc6))
-# print(sol.lengthOfLongestSubstring(tc7))
-sol.lengthOfLongestSubstring(tc3)
-# sol.lengthOfLongestSubstring(tc4)
-# sol.lengthOfLongestSubstring(tc5)
-# sol.lengthOfLongestSubstring(tc6)
-# sol.lengthOfLongestSubstring(tc7)
-
+print(sol.lengthOfLongestSubstring(tc1))
+print(sol.lengthOfLongestSubstring(tc2))
+print(sol.lengthOfLongestSubstring(tc3))
+print(sol.lengthOfLongestSubstring(tc4))
+print(sol.lengthOfLongestSubstring(tc5))
+print(sol.lengthOfLongestSubstring(tc6))
+print(sol.lengthOfLongestSubstring(tc7))
