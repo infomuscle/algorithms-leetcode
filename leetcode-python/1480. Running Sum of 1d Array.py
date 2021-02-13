601618d5b6a82a3bf256e1ad0428684482f70911
infomuscle <infomuscle10@gmail.com>
2021-02-14 01:28:59 +0900

    Runtime 5.77 Memory 74.56

---
+++ leetcode-python/1480. Running Sum of 1d Array.py
@@ -0,0 +1,19 @@
+# 제출 코드 - Runtime 5.77 Memory 74.56
+class Solution:
+    def runningSum(self, nums):
+        answer = [nums[0]]
+
+        for i in range(1, len(nums)):
+            answer.append(answer[-1] + nums[i])
+
+        return answer
+
+
+n1 = [1, 2, 3, 4]
+n2 = [1, 1, 1, 1, 1]
+n3 = [3, 1, 2, 10, 1]
+
+sol = Solution()
+print(sol.runningSum(n1))
+print(sol.runningSum(n2))
+print(sol.runningSum(n3))
