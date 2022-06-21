80242c4f5882951235b0badce47e7b01d84198a8
infomuscle <infomuscle10@gmail.com>
2022-06-22 00:15:00 +0900

    Runtime 17.40 Memory 96.08

--- leetcode-python/547. Number of Provinces.py
+++ leetcode-python/547. Number of Provinces.py
@@ -4,21 +4,19 @@ from collections import deque
 
 class Solution:
     def findCircleNum(self, isConnected):
-
         size = len(isConnected)
-        cnt = 0
 
+        cnt = 0
         for i in range(size):
             for j in range(size):
-                connections_of_i = deque()
                 if isConnected[i][j] == 1:
-                    connections_of_i.append(j)
+                    connections_of_i = deque([j])
                     while connections_of_i:
-                        current = connections_of_i.popleft()
-                        for c, val in enumerate(isConnected[current]):
-                            if isConnected[current][c] == 1:
-                                connections_of_i.append(c)
-                                isConnected[current][c] = 0
+                        current_i = connections_of_i.popleft()
+                        for j_in_i in range(size):
+                            if isConnected[current_i][j_in_i] == 1:
+                                connections_of_i.append(j_in_i)
+                                isConnected[current_i][j_in_i] = 0
                     cnt += 1
 
         return cnt
@@ -33,8 +31,3 @@ c3 = [[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1]]
 print(sol.findCircleNum(c1))
 print(sol.findCircleNum(c2))
 print(sol.findCircleNum(c3))
-
-# 1001
-# 0110
-# 0111
-# 1011
